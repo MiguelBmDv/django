7812d17f36c2bedfe66a83c9288091ae61cbf1e4
MiguelBmDv <129074450+MiguelBmDv@users.noreply.github.com>
2023-11-29 17:38:18 -0500

    update full ins 4,5,6

---
+++ djangoProyecto/todo/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+    path ('admin/', admin.site.urls),
+    path ('',views.home,name="home"),
+    path ("agregar/",views.agregar,name="agregar"),
+    path ("eliminar/<int:tarea_id>/",views.eliminar,name="eliminar"),
+    path ("editar/<int:tarea_id>/", views.editar, name="editar"),
+]
