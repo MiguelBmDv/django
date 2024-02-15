0fbf93795536422a11bf2813f501f8117840b0a1
MiguelBmDv <129074450+MiguelBmDv@users.noreply.github.com>
2024-02-15 11:07:22 -0500

    Django y clase

--- djangoProyecto/todo/urls.py
+++ djangoProyecto/todo/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path ("agregar/",views.agregar,name="agregar"),
     path ("eliminar/<int:tarea_id>/",views.eliminar,name="eliminar"),
     path ("editar/<int:tarea_id>/", views.editar, name="editar"),
+    
+    
+    path ("clase/<int:redirigir>",views.clase,name="clase"),
+    path ("par/",views.par,name="par"),
+    path ("impar/",views.impar,name="impar"),
+    path ("bisciesto/",views.bisciesto,name="bisciesto"),
+    path ("contacto/",views.contacto,name="contacto"),
+    path ("contacto/<str:name>",views.contacto,name="contacto"),
+    path ("contacto/<str:name>/<str:lastname>",views.contacto,name="contacto"),
 ]
--- djangoProyecto/todo/views.py
+++ djangoProyecto/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from .models import Tarea
 from .forms import TareaForm
 
@@ -36,3 +36,84 @@ def editar (request, tarea_id):
         form=TareaForm(instance=tarea)
     context= {'form':form}
     return render (request,'todo/agregar.html',context)
+
+
+
+
+
+
+layout= """
+        <h1> Site Web con Miguel Bm Dev</h1>
+        <hr>
+        <ul>
+            <li><a href="/clase">Años desde el 2024 al 2025</a></li>
+            <li><a href="/par">Años pares</a></li>
+            <li><a href="/impar">Años Impares</li>
+            <li><a href="/bisciesto">Años bisciesto</a></li>
+            <li><a href="/contacto">Contacto</a></li>
+            
+            <br>
+            <li><a href="/">Proyecto Django!</a></li>
+        </ul>      
+        """
+def clase (request,redirigir=0):
+    if redirigir == 1:  
+        return redirect('contacto',name="Miguel")
+    template = """  <h1>Inicio</h1>
+                    <p>Años desde el 2024 al 2050</p>
+                    <ul>
+                """
+    year=2024
+    while year <=2050:
+        template += f"<li> {str(year)}   </li>"  
+        year += 1
+    return HttpResponse(layout+template)
+
+def par (request): 
+    template = """</ul> <hr> <br> <p> Años par </p> """
+    year=2024
+    while year <=2050:
+        if year % 2 == 0:
+            template += f" <li> {str(year)} </li>"
+        year += 1
+    return HttpResponse(layout+template)
+
+def impar (request):
+    template = """</ul> <hr> <br> <p> Años impares </p> """    
+    year=2024
+    while year <= 2050:
+        if year %2 != 0:
+            template += f" <li> {str(year)} </li>"
+        year += 1
+    return HttpResponse(layout+template)
+
+def bisciesto (request):
+    template = """</ul> <hr> <br> <p> Años bisciesto </p> """
+    year=2024
+    while year <= 2050:
+        if year % 4 == 0:
+                template += f" <li> {str(year)} </li>"
+        year += 1
+        
+    return HttpResponse(layout+template)
+
+def contacto (request,name="",lastname=""):
+    html=""
+    if name and lastname:
+        html= "<h2> Nombre completo: </h2>"
+        html+= f"<h2> Bienvenido {name} {lastname} </h2>"
+    elif name:
+        html= "<h2> Nombre sin apellido: </h2>"
+        html+= f"<h2> Bienvenido {name} </h2>"
+    elif lastname:
+        html= "<h2> Apellido sin nombre : </h2>"
+        html+= f"<h2> Bienvenido {lastname} </h2>"
+    else:
+        html= "<h2> Sin nombre y apellidos definidos </h2>"
+        
+    
+    contact= """
+            Bienvenido al apartado de contactos :
+            """
+    return HttpResponse(layout+contact+f"<h2>Contacto </2>"+html)
+
